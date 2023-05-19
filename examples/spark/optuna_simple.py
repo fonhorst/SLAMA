b2a2c982d1849f909a0d5665649e87e419597781
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-05-19 13:51:01 +0300

    ---

--- examples/spark/optuna_simple.py
+++ examples/spark/optuna_simple.py
@@ -1,4 +1,5 @@
 import logging
+import pickle
 from logging import config
 from typing import Tuple, Union, Callable
 
@@ -10,10 +11,10 @@ from pyspark.sql import functions as sf
 
 from examples.spark.examples_utils import get_spark_session
 from sparklightautoml.dataset.base import SparkDataset
+from sparklightautoml.dataset.persistence import PlainCachePersistenceManager
 from sparklightautoml.ml_algo.boost_lgbm import SparkBoostLGBM
 from sparklightautoml.utils import logging_config, VERBOSE_LOGGING_FORMAT
-from sparklightautoml.validation.iterators import SparkHoldoutIterator
-
+from sparklightautoml.validation.iterators import SparkHoldoutIterator, SparkFoldsIterator
 
 logging.config.dictConfig(logging_config(level=logging.DEBUG, log_filename='/tmp/slama.log'))
 logging.basicConfig(level=logging.DEBUG, format=VERBOSE_LOGGING_FORMAT)
@@ -44,8 +45,8 @@ def train_test_split(dataset: SparkDataset, test_slice_or_fold_num: Union[float,
         test = dataset.data.where(sf.col(dataset.folds_column) == test_slice_or_fold_num)
 
     train_dataset, test_dataset = dataset.empty(), dataset.empty()
-    train_dataset.set_data(train, dataset.features)
-    test_dataset.set_data(test, dataset.features)
+    train_dataset.set_data(train, dataset.features, roles=dataset.roles)
+    test_dataset.set_data(test, dataset.features, roles=dataset.roles)
 
     return train_dataset, test_dataset
 
@@ -53,14 +54,19 @@ def train_test_split(dataset: SparkDataset, test_slice_or_fold_num: Union[float,
 if __name__ == "__main__":
     spark = get_spark_session()
 
-    # load data
-    ds = SparkDataset()
+    feat_pipe = "lgb_adv"  # linear, lgb_simple or lgb_adv
+    dataset_name = "lama_test_dataset"
 
-    train_ds, test_ds = train_test_split(ds)
+    # load and prepare data
+    ds = SparkDataset.load(
+        path=f"/tmp/{dataset_name}__{feat_pipe}__features.dataset",
+        persistence_manager=PlainCachePersistenceManager()
+    )
+    train_ds, test_ds = train_test_split(ds, test_slice_or_fold_num=4)
 
     # create main entities
-    iterator = SparkHoldoutIterator(train_ds)
-    tuner = ProgressReportingOptunaTuner(n_trials=10, timeout=300)
+    iterator = SparkFoldsIterator(train_ds).convert_to_holdout_iterator()
+    tuner = ProgressReportingOptunaTuner(n_trials=101, timeout=3000)
     ml_algo = SparkBoostLGBM()
     score = ds.task.get_dataset_metric()
 
@@ -68,17 +74,20 @@ if __name__ == "__main__":
     model, oof_preds = tune_and_fit_predict(ml_algo, tuner, iterator)
     test_preds = ml_algo.predict(test_ds)
 
-    #TODO: report trials
-    # tuner.study.trials
+    # reporting trials
+    # TODO: reporting to mlflow
+    # TODO: quality curves on different datasets
+    with open("/tmp/trials.pickle", "wb") as f:
+        pickle.dump(tuner.study.trials, f)
 
     # estimate oof and test metrics
-    oof_metric_value = score(oof_preds.select(
+    oof_metric_value = score(oof_preds.data.select(
         SparkDataset.ID_COLUMN,
         sf.col(ds.target_column).alias('target'),
         sf.col(ml_algo.prediction_feature).alias('prediction')
     ))
 
-    test_metric_value = score(test_preds.select(
+    test_metric_value = score(test_preds.data.select(
         SparkDataset.ID_COLUMN,
         sf.col(ds.target_column).alias('target'),
         sf.col(ml_algo.prediction_feature).alias('prediction')
