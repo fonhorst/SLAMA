5d6130b6fb9a191981906cbc311286f08b5ef615
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-05-23 13:45:56 +0300

    minor refactoring

--- examples/spark/parallel/mlalgo-folds-parallel.py
+++ examples/spark/parallel/mlalgo-folds-parallel.py
@@ -11,10 +11,10 @@ from sparklightautoml.computations.manager import ParallelComputationsManager
 from sparklightautoml.dataset.base import SparkDataset
 from sparklightautoml.dataset.persistence import PlainCachePersistenceManager
 from sparklightautoml.ml_algo.boost_lgbm import SparkBoostLGBM
-from sparklightautoml.utils import logging_config, VERBOSE_LOGGING_FORMAT
+from sparklightautoml.ml_algo.linear_pyspark import SparkLinearLBFGS
+from sparklightautoml.utils import logging_config, VERBOSE_LOGGING_FORMAT, log_exec_timer
 from sparklightautoml.validation.iterators import SparkFoldsIterator
-
-from examples_utils import get_spark_session
+from examples.spark.examples_utils import get_spark_session
 
 logging.config.dictConfig(logging_config(level=logging.DEBUG, log_filename='/tmp/slama.log'))
 logging.basicConfig(level=logging.DEBUG, format=VERBOSE_LOGGING_FORMAT)
@@ -41,7 +41,8 @@ def train_test_split(dataset: SparkDataset, test_slice_or_fold_num: Union[float,
 if __name__ == "__main__":
     spark = get_spark_session()
 
-    feat_pipe = "lgb_adv"  # linear, lgb_simple or lgb_adv
+    feat_pipe = "linear"  # linear, lgb_simple or lgb_adv
+    ml_algo_name = "linear_l2" # linear_l2, lgb
     dataset_name = os.environ.get("DATASET", "lama_test_dataset")
 
     # load and prepare data
@@ -53,16 +54,20 @@ if __name__ == "__main__":
     train_ds, test_ds = train_ds.persist(), test_ds.persist()
 
     # create main entities
+    computations_manager = ParallelComputationsManager(job_pool_size=1)
     iterator = SparkFoldsIterator(train_ds)#.convert_to_holdout_iterator()
-    ml_algo = SparkBoostLGBM(
-        experimental_parallel_mode=True,
-        computations_manager=ParallelComputationsManager(job_pool_size=2)
-    )
+    if ml_algo_name == "lgb":
+        ml_algo = SparkBoostLGBM(experimental_parallel_mode=True, computations_manager=computations_manager)
+    else:
+        ml_algo = SparkLinearLBFGS(default_params={'regParam': [1e-5]}, computations_manager=computations_manager)
+
     score = ds.task.get_dataset_metric()
 
     # fit and predict
-    model, oof_preds = tune_and_fit_predict(ml_algo, DefaultTuner(), iterator)
-    test_preds = ml_algo.predict(test_ds)
+    with log_exec_timer("Model fitting"):
+        model, oof_preds = tune_and_fit_predict(ml_algo, DefaultTuner(), iterator)
+    with log_exec_timer("Model inference"):
+        test_preds = ml_algo.predict(test_ds)
 
     # estimate oof and test metrics
     oof_metric_value = score(oof_preds.data.select(
